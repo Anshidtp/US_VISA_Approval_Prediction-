6de4aca9f43b68670511b9c2f2fcda01e52396f5
Anshidtp <shaanshid07@gmail.com>
2024-09-27 23:39:24 +0530

    project structure and requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa_approval_predication",
+    version="0.0.0",
+    author="aj",
+    author_email="shaanshid07@gmail.com",
+    packages=find_packages()
+)
